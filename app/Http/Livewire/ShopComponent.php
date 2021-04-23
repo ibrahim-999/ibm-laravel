06c5b66d7f1796fcff2fa038628a90b3a83bd9b3
ibrahim-khalaf <ibrahimkhalaf99@gmail.com>
2021-04-24 01:57:30 +0200

    price filter slider

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -12,10 +12,16 @@ class ShopComponent extends Component
 {
     public $sorting;
     public $pagesize;
+    public $min_price;
+    public $max_price;
+
 
     public function mount(){
         $this->sorting = "default";
         $this->pagesize = 12;
+        $this->min_price = 1;
+        $this->max_price = 1000;
+
     }
     public function store($product_id,$product_name,$product_price){
         Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
@@ -27,20 +33,20 @@ class ShopComponent extends Component
     {
         if($this->sorting==='date')
         {
-            $products = Product::orderBy('created_at', 'DESC')->paginate($this->pagesize);
+            $products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('created_at', 'DESC')->paginate($this->pagesize);
         }
 
         else if ($this->sorting==="price")
         {
-            $products = Product::orderBy('regular_price', 'ASC')->paginate($this->pagesize);
+            $products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('regular_price', 'ASC')->paginate($this->pagesize);
         }
         else if ($this->sorting==="price-desc")
         {
-            $products = Product::orderBy('regular_price', 'DESC')->paginate($this->pagesize);
+            $products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('regular_price', 'DESC')->paginate($this->pagesize);
         }
         else
         {
-            $products = Product::paginate($this->pagesize);
+            $products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->paginate($this->pagesize);
         }
         $categories = Category::All();
         return view('livewire.shop-component', ['products'=>$products, 'categories'=>$categories ])->layout("layouts.base");

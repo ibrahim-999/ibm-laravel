7582397acdd088100c1f6c658f5ca7ce4f52d678
ibrahim-khalaf <ibrahimkhalaf99@gmail.com>
2021-04-24 05:04:18 +0200

    wish&cart counts

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -24,10 +24,30 @@ class ShopComponent extends Component
 
     }
     public function store($product_id,$product_name,$product_price){
-        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('success_message', ' Item added to Cart');
         return redirect()->route('product.cart');
     }
+
+    public function addToWishList($product_id,$product_name,$product_price)
+    {
+        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        $this->emitTo('wishlist-count-component','refreshComponent');
+    }
+
+    public function removeFromWishList($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            if($witem->id == $product_id)
+            {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-count-component','refreshComponent');
+                return;
+            }
+        }
+    }
+
     use WithPagination;
     public function render()
     {

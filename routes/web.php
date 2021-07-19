6ee7669ae99dcac4b2f44b01e560340fe3856417
ibrahim-khalaf <ibrahimkhalaf99@gmail.com>
2021-07-19 10:20:36 +0200

    admin-dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
@@ -68,8 +70,8 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 });
 
 //For admin
-
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
+
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
